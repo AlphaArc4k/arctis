385394ee29edd83397cd52ad8b78f69362c51eef
dyslaxic <3xdevvv@gmail.com>
2025-01-08 19:26:27 +0000

    add settings parser

---
+++ crates/arctis/src/config.rs
@@ -0,0 +1,42 @@
+use serde::Deserialize;
+use config::{Config, ConfigError, File, Environment};
+
+#[derive(Debug, Deserialize)]
+pub struct RpcConfig {
+    pub solana_rpc_url: String,
+    pub solana_ws_url: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Settings {
+    pub rpc: RpcConfig,
+}
+
+impl Settings {
+    fn new() -> Result<Self, ConfigError> {
+      // Use ConfigBuilder instead of merge
+      let settings = Config::builder()
+      // Add the config file as a source
+      .add_source(File::with_name("config"))
+      // Optionally add environment variables with a prefix
+      .add_source(Environment::with_prefix("APP"))
+      .build()?; // Build the config
+
+      // Try to deserialize the settings into the Settings struct
+      settings.try_deserialize()
+    }
+}
+
+pub fn get_settings() -> Result<Settings, ConfigError> {
+    // Load settings
+    match Settings::new() {
+      Ok(settings) => {
+        // println!("Loaded settings: {:?}", settings);
+        return Ok(settings);
+      }
+      Err(e) => {
+        println!("Failed to load settings: {:?}", e);
+        return Err(e);
+      }
+  }
+}
