33d7e194a3c3773654ee39d60a93c4ba5681d688
dyslaxic <3xdevvv@gmail.com>
2025-01-08 16:09:51 +0000

    add client

---
+++ crates/sol-lib/src/client.rs
@@ -0,0 +1,8 @@
+use std::sync::Arc;
+
+pub use solana_client::nonblocking::rpc_client::RpcClient;
+
+pub fn get_client(rpc_url: &str) -> Arc<RpcClient> {  
+  let client = RpcClient::new(rpc_url.to_string());
+  Arc::new(client)
+}
